0b573d7cde81810ec948524dc09c1063df7700e5
Stepan Drimak <108022018+Drimak-Stepan@users.noreply.github.com>
2023-01-27 14:22:19 +0200

    fix

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,9 @@
 import { Component } from 'react';
+import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import { Overlay, ModalCss } from './Modal.styled';
 
+const modalRoot = document.querySelector('#modal-root');
 class Modal extends Component {
   componentDidMount() {
     window.addEventListener('keydown', this.handleKeyDown);
@@ -23,12 +25,13 @@ class Modal extends Component {
   };
   render() {
     const { url } = this.props;
-    return (
+    return createPortal(
       <Overlay onClick={this.handleBackdropClick}>
         <ModalCss>
           {<img src={url} width="800" height="600" alt="bigImage" />}
         </ModalCss>
-      </Overlay>
+      </Overlay>,
+      modalRoot
     );
   }
 }
