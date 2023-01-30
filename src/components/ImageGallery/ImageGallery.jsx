243f8364781ba07f70ee5569c888d245ced445b3
Stepan Drimak <108022018+Drimak-Stepan@users.noreply.github.com>
2023-01-30 13:40:18 +0200

    Fix

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -21,5 +21,5 @@ const ImageGallery = ({ items, ...allProps }) => {
 export default ImageGallery;
 
 ImageGallery.propTypes = {
-  items: PropTypes.arrayOf(PropTypes.shape({ id: PropTypes.string.isRequired})),
+  items: PropTypes.arrayOf(PropTypes.shape({ id: PropTypes.number.isRequired})),
 };
